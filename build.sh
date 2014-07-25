c3c3fc46a7751eac9c0bd14e5190e5b2fd58b917
Dan Collins <dan@dcollins.co.nz>
2014-07-25 23:52:46 +1200

    Improved build process and included all text.

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+rm cards.txt
+
 while (( "$#" )); do
-    cat $1 | sed -e 's/\$/\\$/' | while read x; do
-	echo "\createcard{`basename $1 .txt`}{$x}" >> cards.txt
+    cat "$1" | sed -e 's/\$/\\\\$/g' | while read x; do
+	echo "\createcard{`basename "$1" .txt`}{$x}" >> cards.txt
     done
     shift
 done
